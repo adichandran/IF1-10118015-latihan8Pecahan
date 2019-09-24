1cd714587af3fce302749dbdb46c9973d4f71f2d
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:28:21 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan8pecahan/IF1101108015Latihan8pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan8pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program dengan variabel pecahan
+ */
+public class IF1101108015Latihan8pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 123456789.123456789f;
+        d = 1_234_567_8890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
